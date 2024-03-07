d840280201c922ef808f09994945b9acac67a77e
kishan <kishan.dubariya@phenompeople.com>
2024-03-07 23:33:36 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] arg) {
+        RWOnly rwOnly = new RWOnly();
+        // The field rwOnly.member is not visible.
+        rwOnly.setMember("KD");
+        System.out.println(rwOnly.getMember());
+    }
 }
